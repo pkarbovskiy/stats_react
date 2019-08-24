8bce4858bd7b4d8aa980eeb4f5c5b79d31c30b6e
noMoreLimits\nolimits <pavelkarbovskiy@gmail.com>
2019-08-23 20:50:29 -0400

    merge local changes

--- src/pages/VideoPage.tsx
+++ src/pages/VideoPage.tsx
@@ -1,51 +1,8 @@
-import React, { PureComponent } from 'react';
-declare global {
-    interface Window { Twitch: any; }
+import React from 'react';
+
+export default () => {
+    return (
+        <>
+        </>
+    )
 }
-const EMBED_URL = 'https://player.twitch.tv/js/embed/v1.js';
-export class VideoPage extends PureComponent<any, {}> {
-    options = {
-        width: 840,
-        height: 470,
-        video: 464060196,
-        targetElementId: 'player',
-        videoTime: 11488,
-        autoplay: true
-    };
-
-    player: any = null;
-    createEmbedAddListeners(targetElementId: string, props: any): void {
-        this.player = new window.Twitch.Player(targetElementId, { ...props });
-        this.player.setVideo(`v${props.video}`, props.videoTime);
-        this.addEventListeners(props);
-    }
-    addEventListeners(props: any): void {
-        // this.player.addEventListener(window.Twitch.Player.PLAY, () => {
-        // })
-        this.player.addEventListener(window.Twitch.Player.READY, () => {
-            this.player.seek(props.videoTime);
-            this.player.play();
-        })
-    }
-    componentDidMount() {
-        if (window.Twitch && window.Twitch.Player) {
-            this.createEmbedAddListeners(this.options.targetElementId, this.options);
-            return;
-        }
-        const script = document.createElement('script');
-        script.setAttribute('src', EMBED_URL);
-        script.addEventListener('load', () => {
-            this.createEmbedAddListeners(this.options.targetElementId, this.options);
-        });
-
-        document.body.appendChild(script);
-    }
-    render() {
-        return (
-            <>
-                <div id="player" className="player"></div>
-            </>
-        );
-    };
-}
-
