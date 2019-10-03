827e786c904773ede94ea6dcdbf1a546838af24e
noMoreLimits\nolimits <pavelkarbovskiy@gmail.com>
2019-10-03 07:54:35 -0400

    a little better

--- src/components/AutoSkip.tsx
+++ src/components/AutoSkip.tsx
@@ -15,18 +15,46 @@ const Autoskip = ({ deathKillTimers, videoHandler }: DeathKillProps) => {
     let index: number = 0
     function checkAndMoveToNextEvent() {
         let currentTime: number = videoHandler.getCurrentTime()
+        if (index == null) {
+            index = 0
+        }
+        if (currentTime > deathKillTimers[deathKillTimers.length - 1].endTime + 3) {
+            index = deathKillTimers.length - 1
+            videoHandler.pause()
+            return
+        }
+
         if (index >= 0 && index !== deathKillTimers.length) {
+
+            if (currentTime < deathKillTimers[index].endTime - 3 && currentTime > deathKillTimers[index].startTime) {
+                return
+            }
+            if (currentTime > deathKillTimers[index].endTime + 3
+                && deathKillTimers[index + 1]
+                && currentTime > deathKillTimers[index + 1].endTime + 3
+            ) {
+                for (let i = index + 1; index < deathKillTimers.length; i++) {
+                    index = i
+                    if (deathKillTimers[i].endTime < currentTime) {
+                        continue
+                    }
+                    if (deathKillTimers[i].startTime > currentTime) {
+                        break
+                    }
+                }
+            }
+
             if (currentTime > deathKillTimers[index].endTime + 3) {
                 index++
-                videoHandler.seek(deathKillTimers[index].startTime)
             }
+            videoHandler.seek(deathKillTimers[index].startTime)
         }
     }
     useEffect(() => {
         if (!videoHandler) {
             return
         }
-        videoHandler.seek(deathKillTimers[index].startTime)
+
         let id = setInterval(checkAndMoveToNextEvent, 1000)
 
         return () => {
