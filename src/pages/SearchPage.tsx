0fb4dad23de4fab87b5fbb93d53486ed6385f5b9
noMoreLimits\nolimits <pavelkarbovskiy@gmail.com>
2019-10-16 23:10:14 -0400

    start for the search

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,27 +1,47 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import StreamerVideos from '../components/StreamerVideos'
 import { State } from '../reducers/reducers'
 import { connect } from 'react-redux'
+import { setCurrentSearch } from '../actions'
 
-const SearchPage = ({ match, streamers }: { match: any, streamers: any[] }) => (
-    <div className="search_page">
-        {streamers.map(({ streamer, videos }) =>
-            <StreamerVideos streamer={streamer} videos={videos} />
-        )}
-    </div>
-)
+const SearchPage = ({ match, streamers, streamersById, onData }: { streamersById: any; match: any, streamers: any; onData: any }) => {
+    const url = 'http://192.168.232.129:8000'
+    useEffect(() => {
+        fetch(`${url}/api/search?q=${encodeURI(match.location.query.q)}`)
+            .then(data => data.json())
+            .then(data => {
+                onData(data)
+            })
+    }, [])
+    return (
+        <div className="search_page">
+            {/* {streamers.map(({ streamer, videos }) =>
+                <StreamerVideos
+                    streamer={streamersById}
+                    mediaById={streamersById.videosById}
+                    mediaSorted={streamersById.videosSorted}
+                />
+            )} */}
+        </div>
+    )
+}
 
 
 const mapStateToProps = (state: { mainReducer: State }) => {
     return {
-        streamers: state.mainReducer.streamers
+        streamersById: state.mainReducer.streamersById,
+        featuredStreamers: state.mainReducer.featuredStreamers
     }
 }
 
 const mapDispatchToProps = (dispatch: (arg0: any) => {}) => {
-    return {}
-}
+    return {
+        onData: (data: any) => {
+            dispatch(setCurrentSearch(data))
+        }
 
+    }
+}
 export default connect(
     mapStateToProps,
     mapDispatchToProps
