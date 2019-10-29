450a3483fc917839e08508845753a787f9cee8fd
streamsnipersgames <streamsnipersgames@gmail.com>
2019-10-28 23:38:43 -0400

    small formatting

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -53,7 +53,7 @@ const SearchPage = ({ location, searchFromCache, latestVideos, latestVideosById,
                 }
 
             })}
-
+            <br></br><br></br><br></br><br></br><br></br><br></br>
             {latestVideos.length > 0 && (
                 <>
                     <h3>Also check out latest broadcasts:</h3>
