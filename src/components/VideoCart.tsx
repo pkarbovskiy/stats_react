a3dbbc7823cce3c39fcaa9baf2065d70ad211553
noMoreLimits\nolimits <pavelkarbovskiy@gmail.com>
2019-10-29 22:34:43 -0400

    dates additions and env for deploy

--- src/components/VideoCart.tsx
+++ src/components/VideoCart.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Since from './Since'
 import { Link } from 'react-router-dom'
 export type videoCartProps = {
     id: number;
@@ -21,9 +22,10 @@ export type videoCartProps = {
     match_on_player_id: boolean;
     title: string;
     imageId: number;
+    since: any;
 }
 const VideoCart = (props: videoCartProps) => {
-    let { id, streamer, match_on_player_id, player, streamStart, action, videoId, title, imageId = '', timestamp = '', includeAvatar = false, includeStreamerName = false } = props
+    let { id, streamer, match_on_player_id, player, since, videoId, title, imageId = '', timestamp = '', includeAvatar = false, includeStreamerName = false } = props
     videoId = videoId || id
     function error(event: any) {
         event.target.src = "//d38ev7kpu49one.cloudfront.net/static/image.svg"
@@ -34,12 +36,13 @@ const VideoCart = (props: videoCartProps) => {
             <div className="video_cart__streamername">
                 {includeStreamerName ? `${streamer.name}` : null}
             </div>
-            <Link to={`/video/${videoId}/${timestamp ? `timer/${timestamp}/` : ''}`}>
+            <Link to={`/video/${videoId}/${timestamp ? `timer/${timestamp}/` : ''}`} className="video_cart--link">
                 <img className="video_cart--image"
-                onError={error} 
-                src={`https://d38ev7kpu49one.cloudfront.net/${imageId ? imageId : id}.png`} 
-                alt={title === void 0 ? `${player.name}` : title} 
+                    onError={error}
+                    src={`https://d38ev7kpu49one.cloudfront.net/${imageId ? imageId : id}.png`}
+                    alt={title === void 0 ? `${player.name}` : title}
                 />
+                <Since since={since} />
             </Link>
             <div className="video_cart__info">
                 {includeAvatar ?
@@ -48,15 +51,15 @@ const VideoCart = (props: videoCartProps) => {
                     </Link> :
                     ''
                 }
-            {includeStreamerName ?
-                <span className="video_cart__info__description">
-                    {title === void 0 ? <Link to={`/player/${player.id}/${player.slug}`}>{player.name}</Link> : title}
-                </span>
-                :
-                <span className="video_cart__info__name">
-                    {title === void 0 ? <Link to={`/player/${player.id}/${player.slug}`}> {killMsg} {player.name}</Link> : title}
-                </span>
-            }   
+                {includeStreamerName ?
+                    <span className="video_cart__info__description">
+                        {title === void 0 ? <Link to={`/player/${player.id}/${player.slug}`}>{player.name}</Link> : title}
+                    </span>
+                    :
+                    <span className="video_cart__info__name">
+                        {title === void 0 ? <Link to={`/player/${player.id}/${player.slug}`}> {killMsg} {player.name}</Link> : title}
+                    </span>
+                }
             </div>
         </div>
     )
