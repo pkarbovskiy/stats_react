18a464cf378d1a8cf5b844a1f536c40b220077df
noMoreLimits\nolimits <pavelkarbovskiy@gmail.com>
2019-11-10 19:17:48 -0500

    make color lighter

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -51,7 +51,6 @@ const HomePage = ({ streamersById, featuredStreamers, latestVideos, latestVideos
     return (
         <div className="home_page">
             <div className="home_page__info">
-                <svg viewBox="0 0 192 512"><path d="M176 432c0 44.112-35.888 80-80 80s-80-35.888-80-80 35.888-80 80-80 80 35.888 80 80zM25.26 25.199l13.6 272C39.499 309.972 50.041 320 62.83 320h66.34c12.789 0 23.331-10.028 23.97-22.801l13.6-272C167.425 11.49 156.496 0 142.77 0H49.23C35.504 0 24.575 11.49 25.26 25.199z"></path></svg>
                 How to use the site?<br></br>
                 - search your name above to see if you've killed a streamer OR<br></br>
                 - choose a video and toggle AutoSkip to see all the action (kills, deaths, wins)
