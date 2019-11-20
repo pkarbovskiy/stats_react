94ab5d349dc293dfd5b91667e803ea0167a6fbf4
noMoreLimits\nolimits <pavelkarbovskiy@gmail.com>
2019-11-20 18:22:05 -0500

    commit contants

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,30 @@
 export default ''//'http://192.168.232.129:8000'
+
+export const isMobile = navigator.userAgent.toLowerCase().match(/mobile/i)
+
+export const cloudFrontUrl = '//d38ev7kpu49one.cloudfront.net'
+
+export const featuredStreamer8bit = `${cloudFrontUrl}/featured_streamers/::id.png`
+
+export const defaultPic = `${cloudFrontUrl}/static/face.svg`
+// ids for existing 8bit faces
+export const pic8bitFaces = [
+    3337,
+    10654,
+    3485,
+    3429,
+    5010,
+    3372,
+    3476,
+    3603,
+    3150,
+    3426,
+    3524,
+    3316,
+    3473,
+    3365,
+    3306,
+    3591,
+    8370,
+    3510
+]
