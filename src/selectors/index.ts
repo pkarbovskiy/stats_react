0ff798765d3005f85ddb8e5cc4c28c6120b0f587
Karbovskiy, Pavel <pavel.karbovskiy@kronos.com>
2019-11-21 10:40:59 -0500

    updating dependencies and adding select not sure if usefule

--- src/selectors/index.ts
+++ src/selectors/index.ts
@@ -2,8 +2,22 @@ import { createSelector } from 'reselect'
 import { videoCartProps } from '../components/VideoCart'
 // TODO: set proper type
 const videos = (state: any) => state.mainReducer.videos
+const clipsById = (state: any) => state.mainReducer.clipsById
+const clipsSorted = (state: any) => state.mainReducer.clipsSorted
 
 export const videosByDate = createSelector(
     [videos],
     videos => videos.sort((a: videoCartProps, b: videoCartProps) => a.streamStart - b.streamStart)
+)
+
+export const mediaByAction = createSelector(
+    [clipsSorted, clipsById],
+    (clipsSorted, clipsById) => clipsSorted.reduce((acc:{[action:string]: number[]}, id: number) => {
+        if (!acc[clipsById[id].action]) {
+            acc[clipsById[id].action] = []
+        }
+        acc[clipsById[id].action].push(id)
+        acc.all.push(id)
+        return acc
+    },{all:[]})
 )
