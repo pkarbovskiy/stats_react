48c9c807657126ac76c79fd89185e42d7ff63eed
noMoreLimits\nolimits <pavelkarbovskiy@gmail.com>
2020-02-05 21:26:42 -0500

    scroll top

--- src/pages/VideoPage.tsx
+++ src/pages/VideoPage.tsx
@@ -10,6 +10,8 @@ import TopRated from '../components/TopRated'
 
 const VideoPage = ({ match, media, mediaById, onData }:
     { match: any; media: number[]; mediaById: any; onData: any }) => {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
     const [timeline, setTimeline] = useState()
     const [video, setVideo] = useState()
     useEffect(() => {
