1a35fb798e39dccf2fc3f4f7d3efb99ac31e5a70
noMoreLimits\nolimits <pavelkarbovskiy@gmail.com>
2020-03-02 09:33:43 -0500

    fixing events for table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import VideoCart, { videoCartProps } from './VideoCart'
 
-const Table = ({ mediaSorted, mediaById, playersById, classNameProp = '' }: any) => (
+const Table = ({ mediaSorted, mediaById, playersById, gaEvent, classNameProp = '' }: any) => (
     <div className={`table ${Array.isArray(classNameProp) ? classNameProp.join(' ') : classNameProp}`}>
-        {mediaSorted.map((id: number) => <VideoCart key={id} {...mediaById[id]} />)}
+        {mediaSorted.map((id: number) => <VideoCart key={id} {...mediaById[id]} gaEvent={gaEvent} />)}
     </div>
 )
 
