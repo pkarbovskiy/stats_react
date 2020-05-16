e51b181f57d0070d8926fcf7f9c2559d3c1eba16
bruce <bruce.mcelroy@gmail.com>
2020-05-16 17:51:24 -0400

    remove extra div elements

--- src/components/VideoCart.tsx
+++ src/components/VideoCart.tsx
@@ -43,28 +43,26 @@ const VideoCart = (props: videoCartProps) => {
     const playerUrl = `/player/${(player || {}).id}/${(player || {}).slug}`
     return (
         <div className={`${singleColumn ? ' sm:w-full p-2' : 'sm:w-1/2 xl:w-1/4 p-2'}`}>
-            <div>
-                <div>
-                    <div className={`${darkMode ? 'bg-primary-900 text-white text-sm' : 'bg-white text-gray-800'} relative shadow p-2 rounded-lg hover:shadow-lg transition duration-300 ease-in-out`}>
-                            <Link className="h-40 rounded-lg rounded-b-none w-full object-cover mb-2 block relative" to={videoUrl} onClick={() => gaEvents({ eventCategory: `${gaEvent}`, eventAction: 'click', eventLabel: `${videoId}` })}>
-                                <img className="h-40 rounded-lg rounded-b-none w-full object-cover mb-2"
-                                    onError={error}
-                                    src={reactionImage.replace('::id', `${imageId ? imageId : id}`)}
-                                    alt={title === void 0 ? `${player.name}` : title}
-                                />
-                                <Since since={since} />
-                            </Link>
-                            
+            <div className={`${darkMode ? 'bg-primary-900 text-white text-sm' : 'bg-white text-gray-800'} relative shadow p-2 rounded-lg hover:shadow-lg transition duration-300 ease-in-out`}>
+                    <Link className="relative block object-cover w-full h-40 mb-2 rounded-lg rounded-b-none" to={videoUrl} onClick={() => gaEvents({ eventCategory: `${gaEvent}`, eventAction: 'click', eventLabel: `${videoId}` })}>
+                        <img className="object-cover w-full h-40 mb-2 rounded-lg rounded-b-none"
+                            onError={error}
+                            src={reactionImage.replace('::id', `${imageId ? imageId : id}`)}
+                            alt={title === void 0 ? `${player.name}` : title}
+                        />
+                        <Since since={since} />
+                    </Link>
 
-                        <div className={`flex items-center leading-tight ${darkMode ? 'h-8' : 'h-12'} `}>
-                            {avatar ?
-                                <Link to={`/player/${streamer.id}/${streamer.slug}`} onClick={() => gaEvents({ eventCategory: `${gaEvent}:avatar`, eventAction: 'click', eventLabel: `${videoId}` })}>
-                                    <Avatar player={streamer} />
-                                </Link>
-                                :
-                                ''
-                            }
-                            <div>
+
+                    <div className={`flex items-center leading-tight ${darkMode ? 'h-8' : 'h-12'} `}>
+                        {avatar ?
+                            <Link to={`/player/${streamer.id}/${streamer.slug}`} onClick={() => gaEvents({ eventCategory: `${gaEvent}:avatar`, eventAction: 'click', eventLabel: `${videoId}` })}>
+                                <Avatar player={streamer} />
+                            </Link>
+                            :
+                            ''
+                        }
+                        <div>
                             {includeStreamerName ?
                                 <div className={`text-purple-500 font-bold${title != null ? ' title' : ''}`}>
                                     {title === void 0 ? <Link to={playerUrl}>{player.name}</Link> : title}
@@ -73,22 +71,20 @@ const VideoCart = (props: videoCartProps) => {
                                 <div className={`video_cart__info__name${title != null ? ' title' : ''}${avatar ? ' avatar' : ''}`}>
                                     {avatar && c_title ?
                                         <>
-                                            <div className="text-purple-500 font-bold">{streamer.name}</div>
+                                            <div className="font-bold text-purple-500">{streamer.name}</div>
                                             <Link className="text-sm font-medium text-black" to={videoUrl} onClick={() => gaEvents({ eventCategory: `${gaEvent}`, eventAction: 'click', eventLabel: `${videoId}` })}>{c_title}</Link>
                                         </> :
                                         avatar ?
-                                            <div className="text-purple-500 font-bold">{streamer.name}</div> :
-                                                title === void 0 && player.name !== 'victory' ?
+                                            <div className="font-bold text-purple-500">{streamer.name}</div> :
+                                            title === void 0 && player.name !== 'victory' ?
                                                 <Link to={playerUrl} className="text-sm font-medium text-black" onClick={() => gaEvents({ eventCategory: `${gaEvent}:player_url`, eventAction: 'click', eventLabel: `${videoId}` })}>{`${killMsg}`}<br />{`${player.name}`}</Link> :
-                                                    (title ? title : 'Victory Royale')
-                                    }                                         
+                                                (title ? title : 'Victory Royale')
+                                    }
                                 </div>
                             }
-                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
         </div>
     )
 }
